56359b1c45f702a4fa5a6a14db29953368ff9a7e
RahulRamchandran978 <rahulramchandran978@gmail.com>
2025-05-24 12:22:06 +0530

    theme changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -49,7 +49,7 @@ const AboutSection = () => {
                 </div>
               </div>
             </div>
-            <div className='gradient-border p-6 card-hover'>
+            {/* <div className='gradient-border p-6 card-hover'>
               <div className='flex items-start gap-4'>
                 <div className='p-3 rounded-full bg-primary/10'>
                   <Briefcase className='w-6 h-6 text-primary'/>
@@ -61,7 +61,7 @@ const AboutSection = () => {
                   </p>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
